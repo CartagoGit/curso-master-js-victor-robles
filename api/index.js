939fbde3eca569034f4a57c87edc2abf4f9a64d2
Cartago <cartago.relaxingcup@gmail.com>
2022-09-08 22:48:18 +0200

    Issue con dotenv al pillar variables de entorno en la api

--- api/index.js
+++ api/index.js
@@ -1,17 +1,25 @@
 "use strict";
 
-var mongoose = require("mongoose");
-var app = require("./app");
-require("dotenv").config({ path: "./.env" });
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = require("./app");
+
 // Activamos la lectura de variables de entorno
-console.log(process.env);
-var port = process.env.PORT;
+const result = dotenv.config({path : '../api/.env'});
+if (result.error) {
+	throw result.error;
+}
+console.log(result);
+const port = process.env.PORT;
+const mongoUrl = process.env.MONGO_URL;
+// const port = 3700;
+// const mongoUrl = 'mongodb+srv://CartagoMongo:mongo2Sv@cluster0.ww73cd0.mongodb.net/portafoliosApp';
 console.log(port);
-console.log(process.env.MONGO_URL);
+// console.log(mongoUrl);
 mongoose.Promise = global.Promise;
 // mongoose.connect('mongodb://localhost:27017/portafolio',
 mongoose
-	.connect(process.env.MONGO_URL, {
+	.connect(mongoUrl, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
 		useFindAndModify: false,
@@ -23,7 +31,10 @@ mongoose
 		//Creacion del servidor
 		app.listen(port, () => {
 			console.log(
-				"Servidor corriendo correctamente en la url: localhost:" + port
+				"Base de Datos corriendo correctamente en la url: " +
+					mongoUrl +
+					":" +
+					port
 			);
 			console.log(
 				"-----------------------------------------------------------"
@@ -32,6 +43,8 @@ mongoose
 	})
 	.catch((error) => {
 		console.log("T_T_T_T_T_T_T_T_T_T_T_T_T_T_T_T_T_T_T_T_T_T_T_T_T_T_T_T_T_T");
-		console.log("Ha habido un error: " + error);
+		console.log(
+			"Ha habido un error al conectar a la Base de Datos de Mongo: " + error
+		);
 		console.log("T_T_T_T_T_T_T_T_T_T_T_T_T_T_T_T_T_T_T_T_T_T_T_T_T_T_T_T_T_T");
 	});
